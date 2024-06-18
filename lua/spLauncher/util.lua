20c3ed0bd2a21cc028d1cae767eca33a38e9f242
Noah Friedman <noahfriedman2@gmail.com>
2024-06-18 11:35:19 -0400

    Implment overriding configuration with function
    When using `util.workspace` to override a buffer's configuration, a
    function may now be inputted in place of a `spLauncher.Config` table
    value. See the documentation for the `util.workspace` function (`:h
    spLauncher-utils`) for more information

--- lua/spLauncher/util.lua
+++ lua/spLauncher/util.lua
@@ -2,7 +2,7 @@ local M = {}
 
 --- Override the action map for the buffer when a language server attaches with a valid `root_dir`
 ---@param lsp string The language server name
----@param config spLauncher.Config The override configuration for 'workspace' mode to be merged in
+---@param config spLauncher.Config | fun(string): spLauncher.Config The override configuration for 'workspace' mode to be merged in
 ---@param bufnr? integer The buffer whose action map should be modified, defaults to `vim.api.nvim_get_current_buf()`
 function M.workspace(lsp, config, bufnr)
   vim.api.nvim_create_autocmd("LspAttach", {
@@ -11,7 +11,9 @@ function M.workspace(lsp, config, bufnr)
       local client = vim.lsp.get_client_by_id(args.data.client_id)
 
       if client and client.name == lsp and client.config.root_dir ~= nil and client.config.root_dir ~= "" then
-        vim.b[args.buf].spLauncherActionMap = vim.tbl_deep_extend("force", vim.b[args.buf].spLauncherActionMap, config)
+        vim.b[args.buf].spLauncherActionMap = vim.tbl_deep_extend("force", vim.b[args.buf].spLauncherActionMap,
+                                                                  type(config) == "function" and
+                                                                  config(client.config.root_dir) or config)
       end
     end,
   })
